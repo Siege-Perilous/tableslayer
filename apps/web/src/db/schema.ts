2c5ede83fa4dc0804bcb28d47e8c1cfc2b392189
Dave Snider <dave.snider@gmail.com>
2024-06-27 11:04:30 -0400

    set up drizzle with turso

---
+++ apps/web/src/db/schema.ts
@@ -0,0 +1,26 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const usersTable = sqliteTable('users', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').unique().notNull()
+});
+
+export const scenesTable = sqliteTable('scenes', {
+  id: integer('id').primaryKey(),
+  title: text('title').notNull(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  createdAt: text('created_at')
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date())
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
+
+export type InsertScene = typeof scenesTable.$inferInsert;
+export type SelectScene = typeof scenesTable.$inferSelect;
