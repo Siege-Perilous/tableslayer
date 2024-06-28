dbf79db888935f8a7f5d052a5c27ccb965a2559c
Dave Snider <dave.snider@gmail.com>
2024-06-27 23:37:42 -0400

    move db to lib

--- apps/web/src/lib/db/schema.ts
+++ apps/web/src/lib/db/schema.ts
@@ -4,7 +4,8 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const usersTable = sqliteTable('users', {
   id: integer('id').primaryKey(),
   name: text('name').notNull(),
-  email: text('email').unique().notNull()
+  email: text('email').unique().notNull(),
+  address: text('address')
 });
 
 export const scenesTable = sqliteTable('scenes', {
