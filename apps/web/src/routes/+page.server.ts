7c8be7ed84812ae0171689642025520ac1feea66
Dave Snider <dave.snider@gmail.com>
2024-07-08 11:13:04 -0400

    working user/pass auth with lucia

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,10 +1,18 @@
 import { db } from '$lib/db';
-import { scenesTable } from '$lib/db/schema';
+import { scenesTable, usersTable } from '$lib/db/schema';
+import { redirect } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async (event) => {
+  if (!event.locals.user) redirect(302, '/login');
+
+  const userId = event.locals.user.id;
+  const user = await db.select().from(usersTable).where(eq(usersTable.id, userId)).get();
+
   const scenes = await db.select().from(scenesTable);
   return {
+    user,
     scenes
   };
 }) satisfies PageServerLoad;
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
   import { MyCounterButton } from '@tableslayer/ui';
   export let data;
-  console.log(data.scenes[0].title);
 </script>
 
 <h1>Web</h1>
 <MyCounterButton />
+{data.user.email}
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-{#each data.scenes as scene}
-  <h2>{scene.title}</h2>
-  <p>{scene.userId}</p>
-{/each}
