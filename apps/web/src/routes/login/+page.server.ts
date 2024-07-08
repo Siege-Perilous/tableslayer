4cf7f5d6bbfdf72ebdafbee8466af4c19f7a86a6
Dave Snider <dave.snider@gmail.com>
2024-07-08 14:31:52 -0400

    remove login error

--- apps/web/src/routes/login/+page.server.ts
+++ apps/web/src/routes/login/+page.server.ts
@@ -37,50 +37,43 @@ export const actions: Actions = {
       });
     }
 
-    try {
-      const existingUser = await db.select().from(usersTable).where(eq(usersTable.name, username)).get();
+    const existingUser = await db.select().from(usersTable).where(eq(usersTable.name, username)).get();
 
-      if (!existingUser) {
-        return fail(400, {
-          message: 'Incorrect username or password'
-        });
-      }
-
-      const validPassword = await verify(existingUser.passwordHash, password, {
-        memoryCost: 19456,
-        timeCost: 2,
-        outputLen: 32,
-        parallelism: 1
-      });
-
-      if (!validPassword) {
-        // NOTE:
-        // Returning immediately allows malicious actors to figure out valid usernames from response times,
-        // allowing them to only focus on guessing passwords in brute-force attacks.
-        // As a preventive measure, you may want to hash passwords even for invalid usernames.
-        // However, valid usernames can be already be revealed with the signup page among other methods.
-        // It will also be much more resource intensive.
-        // Since protecting against this is non-trivial,
-        // it is crucial your implementation is protected against brute-force attacks with login throttling, 2FA, etc.
-        // If usernames are public, you can outright tell the user that the username is invalid.
-        return fail(400, {
-          message: 'Incorrect username or password'
-        });
-      }
-
-      const session = await lucia.createSession(existingUser.id.toString(), {});
-      const sessionCookie = lucia.createSessionCookie(session.id);
-      event.cookies.set(sessionCookie.name, sessionCookie.value, {
-        path: '.',
-        ...sessionCookie.attributes
-      });
-
-      return redirect(302, '/');
-    } catch (error) {
-      console.log(error);
-      return fail(500, {
-        message: 'An unknown error occurred'
+    if (!existingUser) {
+      return fail(400, {
+        message: 'Incorrect username or password'
       });
     }
+
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1
+    });
+
+    if (!validPassword) {
+      // NOTE:
+      // Returning immediately allows malicious actors to figure out valid usernames from response times,
+      // allowing them to only focus on guessing passwords in brute-force attacks.
+      // As a preventive measure, you may want to hash passwords even for invalid usernames.
+      // However, valid usernames can be already be revealed with the signup page among other methods.
+      // It will also be much more resource intensive.
+      // Since protecting against this is non-trivial,
+      // it is crucial your implementation is protected against brute-force attacks with login throttling, 2FA, etc.
+      // If usernames are public, you can outright tell the user that the username is invalid.
+      return fail(400, {
+        message: 'Incorrect username or password'
+      });
+    }
+
+    const session = await lucia.createSession(existingUser.id.toString(), {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    event.cookies.set(sessionCookie.name, sessionCookie.value, {
+      path: '.',
+      ...sessionCookie.attributes
+    });
+
+    return redirect(302, '/');
   }
 };
