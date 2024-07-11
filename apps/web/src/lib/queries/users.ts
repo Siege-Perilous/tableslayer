6ab3ada9f3fb31e7a764fa3f9db9b251afb8d681
Dave Snider <dave.snider@gmail.com>
2024-07-11 02:24:04 -0400

    API now uses tanstack, server, query and api separation

---
+++ apps/web/src/lib/queries/users.ts
@@ -0,0 +1,15 @@
+import type { SelectUser } from '$lib/db/schema';
+import { createQuery } from '@tanstack/svelte-query';
+
+export const createUsersQuery = () => {
+  return createQuery<SelectUser[], Error>({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const response = await fetch('/api/users');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }
+  });
+};
