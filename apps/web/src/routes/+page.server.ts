5903bdb68c434c7047f8e4037af2a4dce1433ab6
Dave Snider <dave.snider@gmail.com>
2024-07-13 08:26:57 -0400

    basic cloudinary

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,4 +1,5 @@
-import { getUser } from '$lib/server';
+import { getAssetInfo, getUser, uploadImage } from '$lib/server';
+import { v2 as cloudinary } from 'cloudinary';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
@@ -8,7 +9,29 @@ export const load = (async (event) => {
     user = await getUser(userId);
   }
 
+  cloudinary.config({
+    secure: true
+  });
+
+  const publicId = (await uploadImage(
+    'https://www.milwaukeeindependent.com/wp-content/uploads/2022/09/090222_ThereBeDragons_STK_TStudio.jpg'
+  )) as string;
+
+  const color = await getAssetInfo(publicId);
+  console.log('color', color);
+
+  const optimizedImage = cloudinary.url(publicId, {
+    fetch_format: 'auto',
+    quality: 'auto'
+  });
+  console.log('optimizedImage', optimizedImage);
+
+  const image = cloudinary.image(publicId, {
+    effect: 'gen_replace:from_the dragon;to_a wolf;'
+  });
+
   return {
-    user
+    user,
+    image
   };
 }) satisfies PageServerLoad;
