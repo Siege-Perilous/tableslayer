ba040fa125634c48c6bf26f9e3292c381c2a6b96
Dave Snider <dave.snider@gmail.com>
2024-07-14 09:00:24 -0400

    workspace based on random names

--- apps/web/src/lib/server/index.ts
+++ apps/web/src/lib/server/index.ts
@@ -2,3 +2,4 @@ export * from './auth';
 export * from './email';
 export * from './image';
 export * from './user';
+export * from './workspace';
---
+++ apps/web/src/lib/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './createRandomNames';
+export * from './generateSlug';
