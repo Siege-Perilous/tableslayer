09ecb2125397ee5782dcd4358b2795fe8b1ef6d2
Dave Snider <dave.snider@gmail.com>
2024-07-15 21:58:51 -0400

    signup uses superforms

--- apps/web/src/lib/schemas/index.ts
+++ apps/web/src/lib/schemas/index.ts
@@ -3,3 +3,14 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8)
 });
+
+export const signupSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8)
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword']
+  });
