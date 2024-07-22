e8265accf054d77145e63fc8295ea526e06f6426
Dave Snider <dave.snider@gmail.com>
2024-07-21 23:37:12 -0400

    hook to manage invite redirects

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,11 +1,31 @@
+import { getPartyInvitesForCode, isEmailInUserTable } from '$lib/server';
 import { lucia } from '$lib/server/auth';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const sessionId = event.cookies.get(lucia.sessionCookieName);
+  const pathname = event.url.pathname;
+  const searchParams = event.url.searchParams;
+  const redirectUrl = searchParams.get('redirect');
+
   if (!sessionId) {
     event.locals.user = null;
     event.locals.session = null;
+
+    // Logged out users get redirected to login or signup
+    if (pathname.startsWith('/accept-invite/')) {
+      const inviteCode = pathname.split('/').pop() as string;
+      const inviteEmail = await getPartyInvitesForCode(inviteCode);
+      if (inviteEmail) {
+        const userExists = await isEmailInUserTable(inviteEmail.invite.email);
+        const redirectUrl = `/accept-invite/${inviteCode}`;
+        if (userExists) {
+          throw redirect(302, `/login?redirect=${redirectUrl}`);
+        } else {
+          throw redirect(302, `/signup?redirect=${redirectUrl}`);
+        }
+      }
+    }
     return resolve(event);
   }
 
@@ -28,5 +48,16 @@ export const handle: Handle = async ({ event, resolve }) => {
   }
   event.locals.user = user;
   event.locals.session = session;
+
+  // Logged in get redirected to profile
+  if (pathname.startsWith('/accept-invite/') && user) {
+    throw redirect(302, `/profile`);
+  }
+
+  // Redirect to the original URL after login
+  if (redirectUrl) {
+    throw redirect(302, redirectUrl);
+  }
+
   return resolve(event);
 };
