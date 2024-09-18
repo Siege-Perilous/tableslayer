a1901146f9e1dec085292690e20722041354c306
Dave Snider <dave.snider@gmail.com>
2024-09-17 23:35:55 -0400

    cleanup

--- packages/ui/src/lib/components/PropsTable/PropsTable.svelte
+++ packages/ui/src/lib/components/PropsTable/PropsTable.svelte
@@ -3,7 +3,6 @@
   import Markdown from '@magidoc/plugin-svelte-marked';
   export let componentName = '';
 
-  // Function to get component data (props)
   const getComponentData = (componentName) => {
     const component = jsonData.children.find(
       (child) => child.name === componentName && child.variant === 'declaration'
@@ -16,7 +15,6 @@
     return props?.type ?? null;
   };
 
-  // Function to get the props list for reflection types
   const getPropsList = (type) => {
     if (type?.type === 'intersection') {
       // Handle intersection types (e.g., ButtonProps extending HTMLButtonAttributes)
@@ -29,31 +27,27 @@
     return [];
   };
 
-  // Function to extract extended types (e.g., HTMLButtonAttributes) from intersection types
   const getExtendedProps = (type) => {
     if (type?.type === 'intersection') {
       return (
         type.types
           ?.filter((t) => t.type === 'reference')
-          ?.map((t) => `${t.name}`) // You can use t.qualifiedName if needed
+          ?.map((t) => `${t.name}`)
           .join(', ') || ''
       );
     }
     return '';
   };
 
-  // Function to get the comment summary for a prop
   const getComment = (prop) => {
     return prop?.comment?.summary?.map((s) => s.text).join(' ') || '';
   };
 
-  // Function to get the default value for a prop
   const getDefaultValue = (prop) => {
     const defaultTag = prop?.comment?.blockTags?.find((tag) => tag.tag === '@default');
     return defaultTag ? defaultTag.content[0]?.text || '' : '';
   };
 
-  // Fetch component data
   const componentType = getComponentData(componentName);
   const extendedProps = getExtendedProps(componentType);
   const propsList = getPropsList(componentType);
