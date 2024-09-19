53485142b51728aff530294325897a71a3b1845a
Dave Snider <dave.snider@gmail.com>
2024-09-19 09:32:17 -0400

    cleanup and types

--- apps/docs/src/routes/(components)/codeblock/+page.svelte
+++ apps/docs/src/routes/(components)/codeblock/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { CodeBlock, ColorModeCompare, PropsTable } from '@tableslayer/ui';
+  import { CodeBlock, Code, ColorModeCompare, PropsTable } from '@tableslayer/ui';
   const exampleCode = `/* Coloring the themes comes later */
 a:has(> img) {
   border: 20px solid white;
@@ -7,7 +7,27 @@ a:has(> img) {
 </script>
 
 <ColorModeCompare>
-  <CodeBlock code={exampleCode} lang="css" />
+  <div class="flex">
+    <CodeBlock code={exampleCode} lang="css" />
+  </div>
 </ColorModeCompare>
 
 <PropsTable componentName="CodeBlock" />
+
+<ColorModeCompare>
+  <div class="flex">
+    <p>
+      Inline <Code lang="typescript" code={`const a = 1;`} /> code block
+    </p>
+  </div>
+</ColorModeCompare>
+
+<PropsTable componentName="Code" />
+
+<style>
+  .flex {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+</style>
--- packages/ui/src/lib/components/CodeBlock/types.ts
+++ packages/ui/src/lib/components/CodeBlock/types.ts
@@ -1,4 +1,10 @@
 export type CodeBlockProps = {
   code: string;
   lang?: string;
+  variant?: 'default' | 'inline';
+};
+
+export type CodeProps = {
+  code: string;
+  lang?: string;
 };
