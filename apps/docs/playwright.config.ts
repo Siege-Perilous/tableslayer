e9a7714c608ca598e780c8c55804f648cbf6a166
Dave Snider <dave.snider@gmail.com>
2024-10-28 23:54:19 -0400

    testing ci tests

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -1,6 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
 
 const config: PlaywrightTestConfig = {
+  use: {
+    baseURL: process.env.TEST_URL!
+  },
   webServer: {
     command: 'pnpm run build && pnpm run preview --port 8173',
     port: 8173,
