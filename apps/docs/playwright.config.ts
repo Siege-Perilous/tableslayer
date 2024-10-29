a549075efc7029f4efac49b70e5ea76cc98ffdc9
Dave Snider <dave.snider@gmail.com>
2024-10-29 12:07:16 -0400

    debug

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -3,10 +3,12 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { config as dconfig } from 'dotenv';
 
 dconfig({ path: '.env' });
+const baseURL = process.env.BASE_URL || 'http://localhost:8174';
+console.log('Playwright baseURL:', baseURL);
 
 const config: PlaywrightTestConfig = {
   use: {
-    baseURL: process.env.BASE_URL || 'http://localhost:8174'
+    baseURL
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/
--- apps/docs/tests/test.ts
+++ apps/docs/tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected h1', async ({ page, baseURL }) => {
+  console.log('Base URL in test:', baseURL);
   await page.goto('/');
   await expect(page.getByRole('heading', { name: 'Components' })).toBeVisible();
 });
