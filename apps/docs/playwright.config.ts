e8d6460425322c4f98f0e5836192f96721a9d0d6
Dave Snider <dave.snider@gmail.com>
2024-10-29 18:34:05 +0100

    bypass auth for automation (#36)
    * Bypass vercel auth for tests
    
    * test
    
    * adjust secret

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -4,10 +4,17 @@ import { config as dconfig } from 'dotenv';
 
 dconfig({ path: '.env' });
 const baseURL = process.env.BASE_URL || 'http://localhost:5174';
+const isVercel = process.env.BASE_URL && !process.env.BASE_URL.includes('localhost');
 
 const config: PlaywrightTestConfig = {
+  // @ts-expect-error typing
   use: {
-    baseURL
+    baseURL,
+    ...(isVercel && {
+      extraHTTPHeaders: {
+        'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET
+      }
+    })
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/
