8ab74b99b6bbea8e09b1cc9354939c96e934f86c
Dave Snider <dave.snider@gmail.com>
2024-10-29 13:57:29 -0400

    Purposefully break test

--- apps/web/src/routes/login/+page.svelte
+++ apps/web/src/routes/login/+page.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <Panel class="login">
-  <Title as="h1" size="lg">Sign in</Title>
+  <Title as="h1" size="lg">Blah</Title>
   <Spacer size={2} />
   <p>Don't have an account? <Link href="/signup">Create an account</Link>.</p>
   <Spacer size={8} />
