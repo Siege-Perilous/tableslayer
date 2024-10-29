426c70e3c9204e19537514aac06ac153c9c1197b
Dave Snider <dave.snider@gmail.com>
2024-10-28 23:26:34 -0400

    different method for env

--- apps/web/drizzle.config.app.ts
+++ apps/web/drizzle.config.app.ts
@@ -6,8 +6,7 @@ config({ path: '.env' });
 export default defineConfig({
   schema: './src/lib/db/app',
   out: './migrations/app',
-  dialect: 'sqlite',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
     url: process.env.TURSO_APP_DB_URL!,
     authToken: process.env.TURSO_APP_DB_AUTH_TOKEN!
