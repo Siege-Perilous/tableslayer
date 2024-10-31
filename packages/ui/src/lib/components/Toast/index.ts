2abffc2440b51fd55c1c75f1581df0c01022b68a
Dave Snider <dave.snider@gmail.com>
2024-10-31 04:02:22 +0100

    Toast component (#42)
    * Toast component
    
    * Toast component
    
    * double lint/ts ignore ftw!
    
    * add tests

---
+++ packages/ui/src/lib/components/Toast/index.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck https://github.com/sveltejs/svelte/issues/7304
+export { default as Toast, addToast } from './Toast.svelte';
+export * from './types';
