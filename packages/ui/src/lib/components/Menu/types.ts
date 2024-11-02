28fa54882cc4add104dddc7e8ab6024b52bee2e8
Dave Snider <dave.snider@gmail.com>
2024-11-01 23:25:03 -0400

    ts

--- packages/ui/src/lib/components/Menu/types.ts
+++ packages/ui/src/lib/components/Menu/types.ts
@@ -1,6 +1,5 @@
-import type { CreateMenuProps } from '@melt-ui/svelte';
+import type { FloatingConfig } from '@melt-ui/svelte/internal/actions';
 import type { Snippet } from 'svelte';
-import type { HTMLButtonAttributes } from 'svelte/elements';
 
 export type RadioMenuProps = {
   trigger: Snippet;
@@ -9,26 +8,6 @@ export type RadioMenuProps = {
     value: string;
     href?: string;
   }[];
-  defaultItem: RadioMenuItemProps['items'];
-  positioning: CreateMenuProps['positioning'];
+  defaultItem: RadioMenuProps['items'];
+  positioning: FloatingConfig;
 };
-
-export type MenuItemProps = {
-  children: Snippet;
-  /**
-   * Renders inside of a flex before the children.
-   */
-  start?: Snippet;
-  /**
-   * Renders inside of a flex after the children.
-   */
-  end?: Snippet;
-  isLoading?: boolean;
-  isDisabled?: boolean;
-  /**
-   * Size of the button.
-   * @default md
-   */
-  size?: 'sm' | 'md' | 'lg';
-  href?: string;
-} & HTMLButtonAttributes;
