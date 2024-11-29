135e63754c2d0498c3db8c2a42d04d5d5bce8cca
Dave Snider <dave.snider@gmail.com>
2024-11-29 14:49:11 -0500

    add vercel pr id to app.d.ts

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -18,6 +18,10 @@ declare global {
   }
 }
 
+declare module '$env/dynamic/private' {
+  export const VERCEL_GIT_PULL_REQUEST_ID: string;
+}
+
 declare module '$env/static/private' {
   export const TURSO_API_TOKEN: string;
   export const TURSO_APP_DB_URL: string;
