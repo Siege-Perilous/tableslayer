c1d5a8d4f63a6ed7979aea4a3ea535ad379b7fd1
Daniel Greenheck <dan.greenheck@drgsoftwaresolutions.com>
2024-12-13 17:05:09 -0600

    Update apps/docs/src/routes/(components)/stage/+page.svelte
    Co-authored-by: Dave Snider <dave.snider@gmail.com>

--- apps/docs/src/routes/(components)/stage/+page.svelte
+++ apps/docs/src/routes/(components)/stage/+page.svelte
@@ -176,6 +176,7 @@
     if (e.shiftKey) {
       stageProps.map.zoom = Math.max(minZoom, Math.min(stageProps.map.zoom - scrollDelta, maxZoom));
     } else if (e.ctrlKey) {
+      e.preventDefault();
       stageProps.scene.zoom = Math.max(minZoom, Math.min(stageProps.scene.zoom - scrollDelta, maxZoom));
     }
   }
