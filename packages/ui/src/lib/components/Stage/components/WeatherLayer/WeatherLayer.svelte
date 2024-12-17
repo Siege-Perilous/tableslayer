f7d468d97befd3ec6d27026130425244343a45ca
Daniel Greenheck <dgreenheck@gmail.com>
2024-12-17 14:51:19 -0600

    Refactored WeatherMaterial into a Svelte component

--- packages/ui/src/lib/components/Stage/components/WeatherLayer/WeatherLayer.svelte
+++ packages/ui/src/lib/components/Stage/components/WeatherLayer/WeatherLayer.svelte
@@ -1,40 +1,17 @@
 <script lang="ts">
-  import * as THREE from 'three';
-  import { T, useTask } from '@threlte/core';
+  import { T } from '@threlte/core';
   import { type WeatherProps } from './types';
-  import { WeatherMaterial } from '../../materials/WeatherMaterial';
-  import { onMount } from 'svelte';
+  import WeatherMaterial from '../../materials/WeatherMaterial.svelte';
 
   interface Props {
     props: WeatherProps;
-    z: number;
     resolution: { x: number; y: number };
   }
 
-  const { props, z, resolution }: Props = $props();
-  let time = $state(0);
-
-  // svelte-ignore non_reactive_update
-  let quad: THREE.Mesh;
-  let material = new WeatherMaterial(props);
-
-  onMount(() => {
-    if (quad) {
-      quad.material = material;
-    }
-  });
-
-  $effect(() => {
-    material.uniforms.uResolution.value = new THREE.Vector2(resolution.x, resolution.y);
-    material.updateProps(props);
-  });
-
-  useTask((dt) => {
-    time += dt;
-    material.uniforms.uTime.value = time;
-  });
+  const { props, resolution }: Props = $props();
 </script>
 
-<T.Mesh bind:ref={quad} position={[0, 0, z]} scale={[resolution.x, resolution.y, 1]}>
+<T.Mesh scale={[resolution.x, resolution.y, 1]}>
+  <WeatherMaterial {props} {resolution} />
   <T.PlaneGeometry />
 </T.Mesh>
