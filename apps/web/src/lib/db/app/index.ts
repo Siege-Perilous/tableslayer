1bdc936e8a3c01ea50c0d2a203c86abbaa326e72
Dave Snider <dave.snider@gmail.com>
2024-12-18 08:29:25 -0500

    remove embedded db

--- apps/web/src/lib/db/app/index.ts
+++ apps/web/src/lib/db/app/index.ts
@@ -4,10 +4,15 @@ import { drizzle } from 'drizzle-orm/libsql';
 
 config({ path: '.env' });
 
-const client = createClient({
+/* const client = createClient({
   url: 'file:src/lib/db/app/local.db',
   syncUrl: process.env.TURSO_APP_DB_URL!,
   syncInterval: 30,
+  authToken: process.env.TURSO_APP_DB_AUTH_TOKEN!
+}); */
+
+const client = createClient({
+  url: process.env.TURSO_APP_DB_URL!,
   authToken: process.env.TURSO_APP_DB_AUTH_TOKEN!
 });
 
