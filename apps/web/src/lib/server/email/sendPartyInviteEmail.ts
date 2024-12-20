05004ad540b972dac60d0db396d4dd84dde6eb87
Dave Snider <dave.snider@gmail.com>
2024-12-20 16:08:05 +0100

    Migrate host from Vercel to Fly (#106)

--- apps/web/src/lib/server/email/sendPartyInviteEmail.ts
+++ apps/web/src/lib/server/email/sendPartyInviteEmail.ts
@@ -1,10 +1,9 @@
-import { BASE_URL } from '$env/static/private';
 import { getParty, sendSingleEmail } from '$lib/server';
 import { isValidEmail } from '$lib/utils';
 
 export const sendPartyInviteEmail = async (partyId: string, email: string, code: string) => {
   const party = await getParty(partyId);
-  const baseURL = BASE_URL || 'http://localhost:5174';
+  const baseURL = process.env.BASE_URL || 'http://localhost:5174';
 
   if (!party) {
     throw new Error('Party not found, cannot send invite email.');
