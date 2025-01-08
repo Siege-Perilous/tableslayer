ea7103937884080df542b2e498e0f91db4b64dcf
Daniel Greenheck <dgreenheck@gmail.com>
2025-01-08 16:36:44 -0600

    Update app prop mapping to match new fog of war props

--- apps/web/src/lib/utils/buildSceneProps.ts
+++ apps/web/src/lib/utils/buildSceneProps.ts
@@ -22,11 +22,39 @@ export const buildSceneProps = (activeScene: SelectScene): StageProps => {
     },
     fogOfWar: {
       data: activeScene.fogOfWarData,
-      fogColor: activeScene.fogOfWarColor,
       opacity: activeScene.fogOfWarOpacity,
-      toolType: ToolType.Brush,
-      drawMode: DrawMode.Erase,
-      brushSize: 200
+      outline: {
+        color: '#000000',
+        opacity: 1,
+        thickness: 10
+      },
+      tool: {
+        type: ToolType.Brush,
+        size: 200,
+        mode: DrawMode.Erase
+      },
+      edge: {
+        minMipMapLevel: 0,
+        maxMipMapLevel: 7,
+        frequency: { x: 0.0005, y: 0.001, z: 0.0025, w: 0.005 },
+        amplitude: { x: 0.6, y: 0.5, z: 0.5, w: 0.5 },
+        offset: 0.8,
+        speed: 0.2
+      },
+      noise: {
+        baseColor: activeScene.fogOfWarColor,
+        fogColor1: '#888888',
+        fogColor2: '#aaaaaa',
+        fogColor3: '#cccccc',
+        fogColor4: '#eeeeee',
+        speed: { x: -0.015, y: 0.01, z: -0.05, w: 0.1 },
+        frequency: { x: 0.0008, y: 0.002, z: 0.001, w: 0.001 },
+        offset: { x: -0.4, y: -0.2, z: -0.3, w: -0.25 },
+        amplitude: { x: 0.6, y: 0.5, z: 0.5, w: 1 },
+        persistence: { x: 0.4, y: 0.4, z: 0.3, w: 0.48 },
+        lacunarity: { x: 2.5, y: 2.5, z: 2.5, w: 2 },
+        levels: { x: 6, y: 4, z: 3, w: 6 }
+      }
     },
     grid: {
       gridType: activeScene.gridType as GridType,
