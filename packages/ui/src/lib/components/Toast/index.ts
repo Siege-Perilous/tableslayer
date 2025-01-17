1084dc40d763f4792d212cf0cee616575c98d50e
Dave Snider <dave.snider@gmail.com>
2025-01-17 17:44:28 -0500

    linting

--- packages/ui/src/lib/components/Toast/index.ts
+++ packages/ui/src/lib/components/Toast/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck https://github.com/sveltejs/svelte/issues/7304
 export { default as Toast, addToast } from './Toast.svelte';
 export * from './toastCookie';
 export * from './types';
