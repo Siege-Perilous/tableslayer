80a2ce7887e969957512bae4bb1cf6a7526b296e
Dave Snider <dave.snider@gmail.com>
2025-01-17 12:42:06 -0500

    fix button types

--- packages/ui/src/lib/components/Button/types.ts
+++ packages/ui/src/lib/components/Button/types.ts
@@ -1,28 +1,27 @@
 import type { Snippet } from 'svelte';
 import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
 
-export type ButtonProps =
-  | ({
-      children: Snippet;
-      /**
-       * Renders inside of a flex before the children.
-       */
-      start?: Snippet;
-      /**
-       * Renders inside of a flex after the children.
-       */
-      end?: Snippet;
-      isLoading?: boolean;
-      isDisabled?: boolean;
-      /**
-       * Size of the button.
-       * @default md
-       */
-      size?: 'sm' | 'md' | 'lg';
-      variant?: 'primary' | 'ghost' | 'danger' | 'link';
-      href?: string;
-    } & HTMLButtonAttributes)
-  | HTMLAnchorAttributes;
+export type ButtonProps = {
+  children: Snippet;
+  /**
+   * Renders inside of a flex before the children.
+   */
+  start?: Snippet;
+  /**
+   * Renders inside of a flex after the children.
+   */
+  end?: Snippet;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  /**
+   * Size of the button.
+   * @default md
+   */
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'primary' | 'ghost' | 'danger' | 'link';
+  href?: string;
+} & HTMLButtonAttributes &
+  HTMLAnchorAttributes;
 
 export type IconButtonProps = {
   children: Snippet;
