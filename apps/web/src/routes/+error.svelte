3ff0dfe8d19024c2987b21034d1537d1b7fc2a01
Dave Snider <dave.snider@gmail.com>
2025-01-22 10:22:41 -0500

    Expected errors for 404 (#145)

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import { Title, Text } from '@tableslayer/ui';
+  console.log(page);
+</script>
+
+<div class="error">
+  <Title as="h1" size="xl">
+    {page.status}
+  </Title>
+  <Text color="var(--fgMuted)" size="2rem">
+    {page.error.message}
+  </Text>
+</div>
+
+<style>
+  .error {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: var(--contain-smallForm);
+    padding: var(--size-8);
+    margin: 20vh auto auto auto;
+    position: relative;
+    z-index: 5;
+  }
+  @media (max-width: 768px) {
+    :global(.panel.error--login) {
+      margin: 3rem 3rem auto 3rem;
+    }
+  }
+</style>
