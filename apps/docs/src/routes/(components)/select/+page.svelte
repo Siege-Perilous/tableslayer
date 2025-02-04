4ce892def3e218afa90604c75aa408e9811cbbd4
Dave Snider <dave.snider@gmail.com>
2025-02-04 16:43:39 -0500

    Select component refactor (#155)

--- apps/docs/src/routes/(components)/select/+page.svelte
+++ apps/docs/src/routes/(components)/select/+page.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
   import { Select } from '@tableslayer/ui';
   import { Example } from '$lib/components';
-  const options = [
-    { label: 'Option 1', value: 'option1' },
-    { label: 'Option 2', value: 'option2' },
-    { label: 'Option 3', value: 'option3' },
-    { label: 'Option 4', value: 'option4' }
-  ];
   const optionsWithGroups = {
     'Group 1': [
       { label: 'Option 1', value: 'option1' },
@@ -19,7 +13,19 @@
   };
 </script>
 
+{#snippet labelOne()}
+  <div style="background: yellow">hello</div>
+{/snippet}
+
 <Example title="Avatar" propsName="Avatar">
-  <Select {options} />
-  <Select options={optionsWithGroups} />
+  <Select
+    options={[
+      { label: labelOne, value: 'option1' },
+      { label: 'Option 2', value: 'option2' },
+      { label: 'Option 3', value: 'option3' },
+      { label: 'Option 4', value: 'option4' }
+    ]}
+    selected={[]}
+  />
+  <Select options={optionsWithGroups} multiple={true} selected={['option3']} />
 </Example>
