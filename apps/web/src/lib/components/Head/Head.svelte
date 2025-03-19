3b9b158df51947591d08617087c0bca503a64c1f
Dave Snider <dave.snider@gmail.com>
2025-03-18 21:03:47 -0400

    dev logo, rearrange head / favicon

--- apps/web/src/lib/components/Head/Head.svelte
+++ apps/web/src/lib/components/Head/Head.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
   import { type HeadProps } from './types';
   import { page } from '$app/state';
+  import { dev } from '$app/environment';
   let {
     title = 'Table Slayer',
     description = 'Create animated battle maps for in person RPG games',
     image = `https://tableslayer.com/og.png`
   }: HeadProps = $props();
+
+  const logo = dev ? '/logo-dev.png' : '/logo.png';
 </script>
 
 <svelte:head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="180x180" href={logo} />
+  <link rel="icon" type="image/png" sizes="32x32" href={logo} />
+  <link rel="icon" type="image/png" sizes="16x16" href={logo} />
+  <link rel="manifest" href="/site.webmanifest" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>{title}</title>
   <meta name="description" content={description} />
   <meta property="og:title" content={title} />
