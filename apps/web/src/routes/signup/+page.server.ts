3e8bc475304edf160cd853b3e9113789f79e9037
Dave Snider <dave.snider@gmail.com>
2025-03-26 09:22:47 -0400

    open up signup page

--- apps/web/src/routes/signup/+page.server.ts
+++ apps/web/src/routes/signup/+page.server.ts
@@ -3,9 +3,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-  if (process.env.ENV_NAME === 'production') {
-    throw redirect(302, '/');
-  }
   if (event.locals.user) {
     const userId = event.locals.user.id;
     const user = await getUser(userId);
