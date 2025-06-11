bccf71e9983dc8595634c0a00b8c0572f6f293f2
Dave Snider <dave.snider@gmail.com>
2025-06-11 09:38:11 -0400

    fix vite config for sentry error around env

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,36 +1,38 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig, loadEnv } from 'vite';
 import ws from 'vite-sveltekit-node-ws';
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  plugins: [
-    sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: 'siege-perilous',
-        project: 'tableslayer',
-        authToken: process.env.SENTRY_AUTH_TOKEN
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      sveltekit(),
+      ws(),
+      sentrySvelteKit({
+        sourceMapsUploadOptions: {
+          org: 'siege-perilous',
+          project: 'tableslayer',
+          authToken: env.SENTRY_AUTH_TOKEN
+        }
+      })
+    ],
+    server: {
+      port: 5174,
+      strictPort: false
+    },
+    preview: {
+      port: 4174
+    },
+    test: {
+      include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    optimizeDeps: {
+      exclude: ['@node-rs/argon2', '@node-rs/bcrypt']
+    },
+    build: {
+      commonjsOptions: {
+        include: [/@tableslayer\/ui/, /node_modules/]
       }
-    }),
-    sveltekit(),
-    ws()
-  ],
-  server: {
-    port: 5174,
-    strictPort: false
-  },
-  preview: {
-    port: 4174
-  },
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  },
-  optimizeDeps: {
-    exclude: ['@node-rs/argon2', '@node-rs/bcrypt']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/@tableslayer\/ui/, /node_modules/]
     }
-  }
+  };
 });
