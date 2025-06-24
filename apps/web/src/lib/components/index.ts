de3f05087b1279dd389b6a5bed864815fd6da2ec
Dave Snider <dave.snider@gmail.com>
2025-06-24 13:11:46 -0400

    remove extra component

--- apps/web/src/lib/components/index.ts
+++ apps/web/src/lib/components/index.ts
@@ -1,6 +1,5 @@
 export * from './GameSession';
 export { default as GlobalLoading } from './GlobalLoading.svelte';
-export { default as GoogleAuthButton } from './GoogleAuthButton.svelte';
 export * from './Head';
 export * from './Illustration';
 export * from './party';
