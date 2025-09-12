bc17e710aae70ddea17eadc8eb7ae2e215f161ba
Dave Snider <dave.snider@gmail.com>
2025-09-12 08:38:11 -0400

    Fathom private analytics

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -3,8 +3,9 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
   const envName = process.env.ENV_NAME || 'development';
+  const fathomSiteId = process.env.FATHOM_SITE_ID || '';
 
-  if (!event.locals.user) return { envName };
+  if (!event.locals.user) return { envName, fathomSiteId };
 
   const userId = event.locals.user.id;
   try {
@@ -13,7 +14,8 @@ export const load: LayoutServerLoad = async (event) => {
     return {
       user,
       parties,
-      envName
+      envName,
+      fathomSiteId
     };
   } catch (error) {
     console.error('Error fetching user and parties', error);
