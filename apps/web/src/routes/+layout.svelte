b69f25a343dbc98c11a6af18eacb4b8d4b534495
Dave Snider <dave.snider@gmail.com>
2025-09-19 15:40:46 -0400

    remove fathom

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <!-- I removed conxtext="module" from the script tag, it might be important -->
 <script lang="ts">
-  let { children, data } = $props();
+  let { children } = $props();
   import { QueryClientProvider } from '@tanstack/svelte-query';
   import { queryClient } from '$lib/queryClient';
   import '@tableslayer/ui/styles/globals.css';
@@ -9,12 +9,6 @@
   import { Loader, Toast } from '@tableslayer/ui';
   import { navigating } from '$app/state';
 </script>
-
-<svelte:head>
-  {#if data.envName === 'production' && data.fathomSiteId}
-    <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site={data.fathomSiteId} defer></script>
-  {/if}
-</svelte:head>
 
 <QueryClientProvider client={queryClient}>
   <ModeWatcher defaultMode="dark" darkClassNames={['dark']} lightClassNames={['light']} />
