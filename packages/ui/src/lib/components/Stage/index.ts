4336f2ad0ed452434a8223995524b4c921ec9c67
Dave Snider <dave.snider@gmail.com>
2026-01-10 13:56:40 -0500

    fix ui typings

--- packages/ui/src/lib/components/Stage/index.ts
+++ packages/ui/src/lib/components/Stage/index.ts
@@ -8,20 +8,29 @@ export {
   type AnnotationsLayerProps
 } from './components/AnnotationLayer/types';
 export { DrawMode, RenderMode, ToolType } from './components/DrawingLayer/types';
-export { GridMode, GridType } from './components/GridLayer/types';
-export { MapLayerType } from './components/MapLayer/types';
+export { type EdgeOverlayProps } from './components/EdgeOverlayLayer/types';
+export { type FogLayerProps } from './components/FogLayer/types';
+export { type FogOfWarLayerProps } from './components/FogOfWarLayer/types';
+export { GridMode, GridType, type GridLayerProps } from './components/GridLayer/types';
+export { MapLayerType, type MapLayerProps } from './components/MapLayer/types';
 export {
   MarkerShape,
   MarkerSize,
   MarkerVisibility,
   type Marker,
-  type MarkerLayerExports
+  type MarkerLayerExports,
+  type MarkerLayerProps
 } from './components/MarkerLayer/types';
-export { MeasurementType } from './components/MeasurementLayer/types';
-export { ParticleType } from './components/ParticleSystem/types';
+export { MeasurementType, type MeasurementLayerProps } from './components/MeasurementLayer/types';
+export { ParticleType, type ParticleSystemProps } from './components/ParticleSystem/types';
 export { default as PointerInputManager } from './components/PointerInputManager/PointerInputManager.svelte';
-export { SceneRotation } from './components/Scene/types';
+export { SceneRotation, type PostProcessingProps, type SceneLayerProps } from './components/Scene/types';
 export { default as Stage } from './components/Stage/Stage.svelte';
 export * from './components/Stage/types';
 export { AshPreset, LeavesPreset, RainPreset, SnowPreset } from './components/WeatherLayer/presets';
-export { WeatherType } from './components/WeatherLayer/types';
+export {
+  WeatherType,
+  type DepthOfFieldConfig,
+  type WeatherLayerPreset,
+  type WeatherLayerProps
+} from './components/WeatherLayer/types';
