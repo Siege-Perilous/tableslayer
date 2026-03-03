90e778821f557cbcf40a6f2e97d072b503690f12
Dave Snider <dave.snider@gmail.com>
2026-03-03 11:36:34 -0500

    export toast

--- packages/ui/src/lib/components/Toast/index.ts
+++ packages/ui/src/lib/components/Toast/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck https://github.com/sveltejs/svelte/issues/7304
-export { default as Toast, addToast } from './Toast.svelte';
+export { default as Toast, addToast, removeToast } from './Toast.svelte';
 export * from './toastCookie';
 export * from './types';
